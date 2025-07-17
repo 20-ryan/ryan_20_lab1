f8af0626164b469e80a49ae13f6c7e244d3e8b72
20-ryan <ryantsq@gmail.com>
2025-07-17 12:10:48 +0800

    commit

---
+++ Assets/scrips/spawner.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class spawner : MonoBehaviour
+{
+    public GameObject opponent; 
+    public Text opponentText;
+public int count;
+
+    void Start()
+    {
+        
+        InvokeRepeating("SpawnOpponent", 1f, 2f);
+    }
+
+    void SpawnOpponent()
+    {
+
+        Vector3 position = new Vector3(Random.Range(-10f, 10f), 0.5f, 1f);
+
+
+        Instantiate(opponent, position, Quaternion.identity);
+        count++;
+        opponentText.text = "number of opponents spawned: " + count;
+    }
+}
